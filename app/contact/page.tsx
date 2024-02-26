1bf7174247c0c56fa5f8ee90da40cff98e58a376
mikebashford <mikebashford@gmail.com>
2024-02-25 19:50:46 -0600

    Fixed contact form and made responsive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,42 +1,40 @@
 export default async function Experience() {
   return (
-    <section id="contact">
-      <h5>Get In touch</h5>
-      <h2>Contact Me</h2>
-
-      <div class="container contact__container">
-        <div class="contact__options">
-          <article class="contact__option">
-            <h5>mikebashford&#64;gmail.com</h5>
-          </article>
-          <form>
-            <input
-              type="text"
-              name="name"
-              placeholder="Your Full Name"
-              required
-              ngModel
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Your Email"
-              required
-              ngModel
-            />
-            <textarea
-              name="message"
-              rows="7"
-              placeholder="Your Message"
-              required
-              ngModel
-            ></textarea>
-            <button type="submit" class="btn submit-btn">
-              Send Message
-            </button>
-          </form>
-        </div>
+    <div class="w-3/5 grid grid-cols-1 gap-2 my-5 self-center">
+      <div class="flex flex-col gap-4">
+        <article class="bg-blue-500 p-5 rounded-xl text-center border">
+          <h5>mikebashford&#64;gmail.com</h5>
+        </article>
+        <form className="flex flex-col gap-4">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+            className="w-full p-6 border rounded-xl bg-transparent resize-none bg-white"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            required
+            className="w-full p-6 border rounded-xl bg-transparent resize-none bg-white"
+          />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+            className="w-full p-6 border rounded-xl bg-transparent resize-none bg-white"
+          ></textarea>
+          <button
+            type="submit"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5"
+          >
+            Send Message
+          </button>
+        </form>
       </div>
-    </section>
+    </div>
   );
 }
