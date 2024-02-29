236fb76af051e33b6331cc854bfe6e11e85dd68e
mikebashford <mikebashford@gmail.com>
2024-02-29 13:41:19 -0600

    Fixed contact link on Lets talk button

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
           Download CV
         </a>
         <a
-          href="#contact"
+          href="/contact"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5"
         >
           Let&apos;s Talk
