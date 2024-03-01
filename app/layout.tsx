0a9f5ecc194c26383be856e8d133a9494e24873d
mikebashford <mikebashford@gmail.com>
2024-02-29 21:32:22 -0600

    Added Analytics and SpeedInsights components to site

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
