ca0ced68bacb190a6159f0ffcffe14625ecf193a
mikebashford <mikebashford@gmail.com>
2024-03-01 08:55:16 -0600

    Removed placeholder submit action

--- app/ui/submitButton.tsx
+++ app/ui/submitButton.tsx
@@ -6,7 +6,6 @@ export default function SubmitButton() {
     <button
       type="submit"
       className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded mx-5 w-1/2 self-center max-md:w-3/4"
-      submit={" "}
     >
       Send Message
     </button>
