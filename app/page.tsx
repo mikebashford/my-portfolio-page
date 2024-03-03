f15d11146da618a3fcff2d0a7b73130843b41288
mikebashford <mikebashford@gmail.com>
2024-03-02 18:15:36 -0600

    Removed duplicate import

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import About from "@/app/about/page";
 import Contact from "@/app/contact/page";
 import Skills from "@/app/skills/page";
 import Projects from "@/app/projects/page";
-import Skills from "./skills/page";
 
 export default function Home() {
   return <About />;
