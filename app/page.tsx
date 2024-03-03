553c4e416cbca42093d76ea3503ec675c21268a6
mikebashford <mikebashford@gmail.com>
2024-03-02 18:12:59 -0600

    Corrected path from experience to skills

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import About from "@/app/about/page";
 import Contact from "@/app/contact/page";
-import Experience from "@/app/experience/page";
+import Skills from "@/app/skills/page";
 import Projects from "@/app/projects/page";
+import Skills from "./skills/page";
 
 export default function Home() {
   return <About />;
